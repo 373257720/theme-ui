d20e14bbff0657aa3c8278587e41750227bdb956
Zhanhang Liang <zhanhangliang@ddmarketinghub.com>
2021-04-13 17:50:15 +0800

    4-13

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <Custom-Input></Custom-Input>
+    <GZ-Textarea></GZ-Textarea>
+    <GZ-radio></GZ-radio>
+    <GZ-checkbox></GZ-checkbox>
+  </div>
+</template>
+
+<script>
+import CustomInput from '@/components/input/input'
+import GZTextarea from '@/components/input/textarea'
+import GZradio from '@/components/radio/radio'
+import GZcheckbox from '@/components/checkbox/checkbox'
+// import { component } from 'vue/types/umd'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'Custom-Input': CustomInput,
+    'GZ-Textarea': GZTextarea,
+    'GZ-radio': GZradio,
+    'GZ-checkbox': GZcheckbox
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/input/input.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="GZ-input">
+    <input
+      class="my-number-input"
+      placeholder="please input"
+      @focus="
+        () => {
+          leave = true;
+        }
+      "
+      v-model="content"
+    />
+    <span
+      @click="
+        () => {
+          content = '';
+        }
+      "
+      class="GZ-input__suffix iconfont"
+      :class="{ 'icon-shanchu': content && leave }"
+    >
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+      leave: true,
+    };
+  },
+  methods: {
+    // delectFun (event) {
+    //   this.content = ''
+    // }
+  },
+};
+</script>
+
+<style lang='scss'>
+.GZ-input {
+  width: 180px;
+  position: relative;
+  font-size: 14px;
+  margin-bottom: 30px;
+  // display: inline-block;
+  // vertical-align: bottom;
+  .my-number-input {
+    z-index: $z-Index88;
+    -webkit-appearance: none;
+    background-image: none;
+    display: inline-block;
+    outline: none;
+    transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    @include Font(inherit, 40px, #606266);
+    @include border(1px, solid, #dcdfe6, 4px);
+    @include boxModel(100%, 40px, #fff, 0 30px 0 15px);
+    &:focus {
+      outline: none;
+      border-color: $themeColor;
+    }
+  }
+  .GZ-input__suffix {
+    z-index: $default-Index;
+    color: $themeColor;
+    cursor: pointer;
+    @include positonAbsolute("m", 0, "r", 10px);
+  }
+}
+</style>
